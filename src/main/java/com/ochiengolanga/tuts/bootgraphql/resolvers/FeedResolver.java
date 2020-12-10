ac52488e465e6dcb459bddfbb168814c00a0263c
Ubuntu <ubuntu@virtualserver01.NetHealth-Lab-LLC.cloud>
2020-12-10 21:58:19 +0000

    FeedResolver

--- src/main/java/com/ochiengolanga/tuts/bootgraphql/resolvers/FeedResolver.java
+++ src/main/java/com/ochiengolanga/tuts/bootgraphql/resolvers/FeedResolver.java
@@ -2,9 +2,21 @@ package com.ochiengolanga.tuts.bootgraphql.resolvers;
 
 import com.coxautodev.graphql.tools.GraphQLResolver;
 import com.ochiengolanga.tuts.bootgraphql.domain.Feed;
+import com.ochiengolanga.tuts.bootgraphql.repository.FeedRepository;
 
 import java.util.Optional;
 
 public class FeedResolver implements GraphQLResolver<Feed> {
 
+    private final String NAME_FEEDRESOLVER = "FeedResolver";
+    private final FeedRepository feedRepository;
+
+    public FeedResolver(FeedRepository feedRepository) {
+        String methodName = NAME_FEEDRESOLVER;
+        this.feedRepository = feedRepository;
+    }
+
+    public Optional<Feed> getOne(Feed feed) {
+        return feedRepository.findById(1L);
+    }
 }
