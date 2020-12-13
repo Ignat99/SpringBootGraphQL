95d7ce46a4eb26736f2ef7df88fd405d896bc443
Ubuntu <ubuntu@virtualserver01.NetHealth-Lab-LLC.cloud>
2020-12-13 16:50:09 +0000

    FeedTest

---
+++ src/test/java/com/ochiengolanga/tuts/bootgraphql/domain/entity/FeedTest.java
@@ -0,0 +1,32 @@
+package com.ochiengolanga.tuts.bootgraphql.domain.entity;
+
+//import com.ochiengolanga.tuts.bootgraphql.exceptions.FeedDateException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+//import java.math.BigDecimal;
+
+//import static java.math.BigDecimal.ONE;
+//import static java.math.BigDecimal.TEN;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(JUnit4.class)
+public class FeedTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void giveMoneyMustThrowExceptionWhenAccountBalanceIsGoingNegative() {
+        final Feed feed = Feed.builder().title("Dummy").description("bla bla bla").pubDate("Sun, 13 Dec 2020 16:05:36 +0100").build();
+
+//        thrown.expect(FeedDateException.class);
+
+        feed.getFormattedDate();
+
+        assertThat(feed.getFormattedDate()).isEqualTo("2020-12-13");
+    }
+}
