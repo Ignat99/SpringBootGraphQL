7c14f0accbc9d24d069be165e6d1796be6c0ef61
Ubuntu <ubuntu@virtualserver01.NetHealth-Lab-LLC.cloud>
2020-12-13 01:19:55 +0000

    lambok

--- src/main/java/com/ochiengolanga/tuts/bootgraphql/domain/entity/Feed.java
+++ src/main/java/com/ochiengolanga/tuts/bootgraphql/domain/entity/Feed.java
@@ -5,21 +5,21 @@ import java.util.Objects;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-//import lombok.ToString;
+import lombok.ToString;
 import lombok.Data;
-//import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
 import org.hibernate.annotations.DynamicUpdate;
-import org.hibernate.annotations.NaturalId;
+//import org.hibernate.annotations.NaturalId;
 
 @Data
-//@EqualsAndHashCode
-//@Getter
-//@Setter
-//@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
+@ToString
 @NoArgsConstructor
 @Entity
 @DynamicUpdate
@@ -32,21 +32,9 @@ public class Feed implements Serializable {
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
 
-    @NaturalId(mutable = false)
+//    @NaturalId(mutable = false)
     @Column(name="feed_title", nullable = false, unique = true, length = 512)
     private String title;
-
-    // @NaturalId(mutable = false)
-    // @Column(nullable = false, updatable = false, unique = true)
-    // private Long sku;
-    /*
-    public Long getSku() {
-        return sku;
-    }
-    public void setSku(Long sku) {
-        this.sku = sku;
-    }
-    */
 
     @Column(length = 30000, name="feed_description", nullable = true)
     private String description;
@@ -74,39 +62,4 @@ public class Feed implements Serializable {
     public String getFormattedDate() {
         return getPubDate().toString();
     }
-
-
-    @Override
-    public boolean equals(java.lang.Object o) {
-
-        if (this == o) {
-            return true;
-        }
-
-        if (getClass() != o.getClass()) {
-            return false;
-        }
-
-        Feed other = (Feed) o;
-        return Objects.equals(title, other.getTitle());
-        // including sku 
-        // return Objects.equals(title, other.getTitle())
-        //        && Objects.equals(sku, other.getSku());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title);
-        // including sku
-        // return Objects.hash(title, sku);
-    }
-
-    @Override
-    public String toString() {
-        return "Feed{" + "id=" + id + ", title=" + title + ", pubDate=" + pubDate + ", itemCount=" + itemCount + '}';
-        // including sku
-        //return "Feed{" + "id=" + id + ", title=" + title 
-        //         + ", pubDate=" + pubDate + ", itemCount=" + itemCount + ", sku=" + sku + '}';
-    }
-
 }
