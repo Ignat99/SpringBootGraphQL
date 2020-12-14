6d9ad24a26fa2e494ae9e6de2d6d6d1f0b6f0724
Ubuntu <ubuntu@virtualserver01.NetHealth-Lab-LLC.cloud>
2020-12-14 17:54:29 +0000

    clean NosFeedServiceTest.java

--- src/test/java/com/ochiengolanga/tuts/bootgraphql/service/NosFeedServiceTest.java
+++ src/test/java/com/ochiengolanga/tuts/bootgraphql/service/NosFeedServiceTest.java
@@ -1,26 +1,8 @@
 package com.ochiengolanga.tuts.bootgraphql.service;
 
 import com.ochiengolanga.tuts.bootgraphql.domain.entity.Feed;
-import com.ochiengolanga.tuts.bootgraphql.interfaces.FeedService;
 import com.ochiengolanga.tuts.bootgraphql.repository.FeedRepository;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.ExampleMatcher;
-import org.springframework.data.domain.Example;
-//import com.ochiengolanga.tuts.bootgraphql.utils.LogService;
 import org.springframework.util.StopWatch;
-//import com.ochiengolanga.tuts.bootgraphql.interfaces.FeedService;
-//import org.springframework.beans.factory.annotation.Autowired;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -29,10 +11,6 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-
-import java.time.LocalDateTime;
-import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
@@ -41,7 +19,6 @@ public class NosFeedServiceTest {
 
     @Mock
     private FeedRepository feedRepository;
-
 
     @InjectMocks
     private NosFeedService target;
